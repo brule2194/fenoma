362be678a277dae25941d05af44ab70a583190df
Brule Lazaro Hidalgo Ova <bhidalgoova@gmail.com>
2023-09-23 13:30:15 -0400

    creating route solution

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    item: str
+    quantity: int
+    price: float
+    status: bool
+
+
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/solution")
+def process_orders(orders, criterion):
+    return {"item_name"}
